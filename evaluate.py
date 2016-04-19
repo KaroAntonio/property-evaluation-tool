14123a3e7586a98621d3750108870322e2c3878e
KaroAntonio <karoantonio@gmail.com>
2016-04-19 11:37:48 -0400

    Max Offer

--- evaluate.py
+++ evaluate.py
@@ -9,13 +9,31 @@ r = 0.025						# Mortgage Rate
 p = 25 							# Amortization Period
 
 # Headers to display
-disp = ['cash', 'income', 'cost', 'beds','list', 'address']
+headers = ['cashflow', 'income', 'cost', 'beds','list', 'address']
 
 costs = [insurance, utils]
 
 properties = load_tsv('properties.tsv')
 
-print('\t'.join(disp))
+print('\t'.join(headers))
+
+def get_max_offer(prop, cap):
+	'''
+	prop: property dict
+	cap: limit on max offer
+	Return Max offer that can be made while maintaining +ve cashflow
+	'''
+	offer = prop['list']
+	while True:
+		offer += 10000
+		mp = calc_mortgage_payment( offer - down, r, p )
+		mt = (prop['taxes'] + prop['taxes+']) / 12
+		cost = mp + mt + sum(costs) + prop['fees'] 
+		cashflow = prop['income'] - cost
+		if cashflow < 0 or offer > cap:
+			print(offer, cashflow)
+			return offer - 10000
+
 for prop in properties:
 	# Income (Monthly) 
 	prop['income'] = prop['beds'] * prop['rent']
@@ -31,15 +49,19 @@ for prop in properties:
 	prop['cost'] = mp + mt + sum(costs) + prop['fees'] 
 
 	# Cashflow 
-	prop['cash'] = prop['income'] - prop['cost']
+	prop['cashflow'] = prop['income'] - prop['cost']
+	
+	# Max Offer 
+	prop['max_offer'] = get_max_offer(prop, 700000) 
+	prop['max_offer'] = prop['max_offer'] if prop['cashflow'] > 0 else 0
 
 # Sort According to Cashflow
-properties.sort(key = lambda x: x['cash'], reverse = True)
+properties.sort(key = lambda x: x['cashflow'], reverse = True)
 
 for prop in properties:
 	# Display
 	items = []
-	for d in disp:
+	for d in headers:
 		try: items += [str(int(prop[d]))]
 		except: items += [str(prop[d])]
 	disp_str = "\t".join(items)
--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function build_table(data) {
 	var table = $("<table>",{id:'property_table', class:'bland'});
 	$('#table').append(table)
 	headers = Object.keys(data[0]);
-	ordered = ['link','address','cash','beds','bath'];
+	ordered = ['link','address','cashflow','beds','bath'];
 	// Add elements not in ordered to ordered
 	$(headers).each(function(i){
 		var oi = ordered.indexOf(this+"");
